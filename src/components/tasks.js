657a3e288f24efa5e2d31e9ddfc84671b7562575
Joel Brigida <joel@drtechreps.com>
2022-11-12 21:46:34 -0500

    Create tasks.js

---
+++ src/components/tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text:'Doctors Appointment',
+        day: 'Feb 5th at 2:30 PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30 PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 6th at 2:30 PM',
+        reminder: false,
+    }
+]
+
+const Tasks = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Tasks
