98515f64c1da7a9573637d4a76c43d51013b5c15
Joel Brigida <joel@drtechreps.com>
2022-11-13 17:30:49 -0500

    Create state for adding task text entry

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  
+  // Controls the state of text boxes for Task and Day and Time
+  const [showAddTask, setShowAddTask ] = useState(false)
+  
   const [tasks, setTasks ] = useState([
     {
         id: 1,
@@ -39,7 +43,8 @@ const App = () => {
 
 // Add Task
 const addTask = (task) => {
-  // Generate a random task id /  transaction id / job id
+  // Generate a random task id /  transaction id / job id: 
+  // Need to replace the random generated IDS with an actual database
   const id = Math.floor(Math.random() * 10000) + 1
   const newTask = { id, ...task }
   setTasks([...tasks, newTask])
@@ -60,7 +65,7 @@ const toggleReminder = (id) => {
 return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete=
       {deleteTask} onToggle={toggleReminder}/>
